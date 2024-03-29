2f9c3ea3f135a3a78dac3dc6b5e0147e2b4363af
Gaurav <gaurav.info217@gmail.com>
2024-03-29 09:47:41 +0530

    setup server

---
+++ server/app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const cookieParser = require('cookie-parser')
+const morgan = require('morgan');
+const createError = require('http-errors');
+const connectDB = require('./config/init_db');
+const app = express();
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+const MONGOURI = process.env.MONGODB_URI;
+
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+app.use(morgan('dev'));
+
+app.use(async(req,res,next) => {
+    next(createError.NotFound());
+})
+
+app.use(async(err,req,res,next) => {
+    res.status(err.status || 500).send({
+        status : err.status || 500,
+        message : err.message
+    })
+});
+
+const startServer = () => {
+    connectDB(MONGOURI)
+    .then((conn) => {
+        console.log('Database connected')
+        app.listen(PORT,() => {
+            console.log(`Server started at port :: ${PORT}`);
+        })
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+};
+
+startServer();
